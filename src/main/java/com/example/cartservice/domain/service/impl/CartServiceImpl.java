8e36d2b469d3b9c1c87a1ed0779a79de1f353444
anushan1508 <anushan1508@gmail.com>
2024-03-13 02:51:53 +0530

    first

--- src/main/java/com/example/cartservice/domain/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/cartservice/domain/service/impl/CartServiceImpl.java
@@ -5,10 +5,15 @@ import com.example.cartservice.domain.dto.addCart.AddCartResponse;
 import com.example.cartservice.domain.dto.removeCart.RemoveCartRequest;
 import com.example.cartservice.domain.dto.removeCart.RemoveCartResponse;
 import com.example.cartservice.domain.service.CartService;
+import com.example.cartservice.external.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartServiceImpl implements CartService {
+    @Autowired
+    CartRepository cartRepository;
+
     @Override
     public AddCartResponse addToCart(AddCartRequest addCartRequest) {
         return null;
