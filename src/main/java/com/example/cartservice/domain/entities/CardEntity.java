020e8eb0740705ec91aab9debb3a92ba794ea63b
anushan1508 <anushan1508@gmail.com>
2024-03-13 02:56:27 +0530

    first

--- src/main/java/com/example/cartservice/domain/entities/CardEntity.java
+++ src/main/java/com/example/cartservice/domain/entities/CardEntity.java
@@ -1,4 +1,22 @@
 package com.example.cartservice.domain.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cart", indexes = {
+        @Index(name = "cartItem", columnList = "cartItem")
+})
 public class CardEntity {
+    @Id
+    private Long id;
+    private String cartItem;
 }
--- src/main/java/com/example/cartservice/external/repository/CartRepository.java
+++ src/main/java/com/example/cartservice/external/repository/CartRepository.java
@@ -1,7 +1,9 @@
 package com.example.cartservice.external.repository;
 
+import com.example.cartservice.domain.entities.CardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository {
+public interface CartRepository extends JpaRepository<CardEntity, Long> {
 }
